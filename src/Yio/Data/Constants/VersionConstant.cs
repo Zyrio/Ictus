a52caec2a3140a50568963a51e5232e7070ace81
Ducky <ducky.vexton@outlook.com>
2017-09-08 09:56:54 +0100

    Bump version to 1702.2 (17.2.2)

--- src/Yio/Data/Constants/VersionConstant.cs
+++ src/Yio/Data/Constants/VersionConstant.cs
@@ -3,7 +3,7 @@ namespace Yio.Data.Constants {
     public class VersionConstant
     {
         public static int Release = 1702;
-        public static int Patch = 1;
+        public static int Patch = 2;
         public static string Codename = "Kinky Kiwi";
         public static bool Unstable = false;
     }
