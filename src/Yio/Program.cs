a4aaa9f827d8b04d45411650c6f0282fb8324755
Ducky <ducky.vexton@outlook.com>
2017-09-08 01:37:42 +0100

    Fix server not starting on correct URL

--- src/Yio/Program.cs
+++ src/Yio/Program.cs
@@ -46,6 +46,7 @@ namespace Yio
 
         public static IWebHost BuildWebHost(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
+                .UseUrls("http://0.0.0.0:" + _port.ToString() + "/")
                 .UseStartup<Startup>()
                 .Build();
     }
