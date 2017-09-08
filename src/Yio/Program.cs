7de732361e35815e3ff6aed2e41c96542108ce03
Ducky <ducky.vexton@outlook.com>
2017-09-08 02:17:57 +0100

    Fix port in arguments crashing app

--- src/Yio/Program.cs
+++ src/Yio/Program.cs
@@ -39,13 +39,13 @@ namespace Yio
             }
 
             StartupUtilities.WriteInfo("starting App");
-            BuildWebHost(args).Run();
+            BuildWebHost().Run();
 
             Console.ResetColor();
         }
 
-        public static IWebHost BuildWebHost(string[] args) =>
-            WebHost.CreateDefaultBuilder(args)
+        public static IWebHost BuildWebHost() =>
+            WebHost.CreateDefaultBuilder()
                 .UseUrls("http://0.0.0.0:" + _port.ToString() + "/")
                 .UseStartup<Startup>()
                 .Build();
