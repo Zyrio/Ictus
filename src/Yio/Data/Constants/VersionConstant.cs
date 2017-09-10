f47a65d397f8dcbe75eed84641ce98de32de8162
Ducky <ducky.vexton@outlook.com>
2017-09-10 06:38:58 +0100

    Bump version to 1703.6

--- src/Yio/Data/Constants/VersionConstant.cs
+++ src/Yio/Data/Constants/VersionConstant.cs
@@ -3,7 +3,7 @@ namespace Yio.Data.Constants {
     public class VersionConstant
     {
         public static int Release = 1703;
-        public static int Patch = 5;
+        public static int Patch = 6;
         public static string Codename = "Dazzling Duck";
         public static bool Unstable = false;
     }
