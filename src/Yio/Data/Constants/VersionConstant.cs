99d029f632d99085ecdf50bf5b5839135bbe84c6
Ducky <ducky.vexton@outlook.com>
2017-09-10 08:33:42 +0100

    Fix cache issues

--- src/Yio/Data/Constants/VersionConstant.cs
+++ src/Yio/Data/Constants/VersionConstant.cs
@@ -3,7 +3,7 @@ namespace Yio.Data.Constants {
     public class VersionConstant
     {
         public static int Release = 1704;
-        public static int Patch = 1;
+        public static int Patch = 2;
         public static string Codename = "Zesty Zebra";
         public static bool Unstable = false;
     }
