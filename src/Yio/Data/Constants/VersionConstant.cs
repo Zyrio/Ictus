049eba4a87c23028c0c68ea3062d14c46a160194
Ducky <ducky.vexton@outlook.com>
2017-09-11 17:31:13 +0100

    Bump version to 1706.1 (17.6.1)

--- src/Yio/Data/Constants/VersionConstant.cs
+++ src/Yio/Data/Constants/VersionConstant.cs
@@ -3,7 +3,7 @@ namespace Yio.Data.Constants {
     public class VersionConstant
     {
         public static int Release = 1706;
-        public static int Patch = 0;
+        public static int Patch = 1;
         public static string Codename = "Lewd Lion";
         public static bool Unstable = false;
     }
