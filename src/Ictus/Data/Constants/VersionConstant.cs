0151c0bb716004f0059b7bd73c26c7c63f35b1cf
Ducky <ducky.vexton@outlook.com>
2017-09-25 03:20:23 +0100

    Bump version to 1708.9 (17.8.9)

--- src/Ictus/Data/Constants/VersionConstant.cs
+++ src/Ictus/Data/Constants/VersionConstant.cs
@@ -3,7 +3,7 @@ namespace Ictus.Data.Constants {
     public class VersionConstant
     {
         public static int Release = 1708;
-        public static int Patch = 8;
+        public static int Patch = 9;
         public static string Codename = "Twinky Tiger";
         public static bool Unstable = false;
     }
